e756cf6815e9b1e74398bce51f89d6fa5c1bab96
Aditya Sinha <iadityasinha@gmail.com>
2013-08-11 01:41:02 +0530

    Course deletion support

--- CDCourses/CDCourses/Classes/ASCoursesTableViewController.m
+++ CDCourses/CDCourses/Classes/ASCoursesTableViewController.m
@@ -102,19 +102,23 @@
  }
  */
 
-/*
+
  // Override to support editing the table view.
  - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
  {
  if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+     
+     Course *courseToDelete = [_fetchedResultsController objectAtIndexPath:indexPath];
+     [self.managedObjectContext deleteObject:courseToDelete];
+     
+     NSError *error = nil;
+     if (! [self.managedObjectContext save:&error]) {
+         NSLog(@"Error Saving: %@",error);
+     }
  }
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+
  }
- }
- */
+ 
 
 /*
  // Override to support rearranging the table view.
